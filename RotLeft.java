aeb141f53750730d88498cc48b58adec0de72e1f
Chizara06 <129799084+Chizara06@users.noreply.github.com>
2024-06-11 14:02:35 +0100

    Add files via upload
    IKE ZARA PHOENIX  BHU/22/04/10/0003

---
+++ RotLeft.java
@@ -0,0 +1,10 @@
+public class RotLeft {
+    public static void main(String[] args) {
+        
+        System.out.println("Original Array: ");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+
+    }
+}
